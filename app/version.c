c23e6ae35ef6d3fd6a2f3f3219b94054c2f8517c
Joe Allen <jhallenworld@gmail.com>
2021-12-07 12:26:00 -0500

    Makefile work

--- app/version.c
+++ app/version.c
@@ -1,9 +1,9 @@
 #include "version.h"
 
-const uint16_t firmware_major = NKVERSION_MAJOR;
-const uint16_t firmware_minor = NKVERSION_MINOR;
-const uint16_t build_year = NKYEAR;
-const uint8_t build_month = NKMONTH;
-const uint8_t build_day = NKDAY;
-const uint8_t build_hour = NKHOUR;
-const uint8_t build_minute = NKMINUTE;
+const uint16_t firmware_major = NK_VERSION_MAJOR;
+const uint16_t firmware_minor = NK_VERSION_MINOR;
+const uint16_t build_year = NK_YEAR;
+const uint8_t build_month = NK_MONTH;
+const uint8_t build_day = NK_DAY;
+const uint8_t build_hour = NK_HOUR;
+const uint8_t build_minute = NK_MINUTE;
