22b49f3b13891d1c5c4c361112b248b5b650b540
Joe Allen <jhallenworld@gmail.com>
2022-02-10 11:17:52 -0500

    Fix comments

--- inc/nkcli.h
+++ inc/nkcli.h
@@ -26,19 +26,27 @@
 #include "nkscan.h"
 #include "nkprintf.h"
 
-#define CMD_FLAG_HIDDEN 1 // Command is hidden unless in "factory service mode"
+// Command table entry
 
 struct console_cmd {
     const char *text; // Command name and help
     int (*func)(nkinfile_t *args); // Command function
 };
 
-// Macros to help declare commands
-// There is a section dedicated to the command table.  These macros add entries to this table.
-// This way we avoid having a single file with a giant command table.  Instead the command registration and help can be placed right next to the command function
+// Macro for declaring a command
+
+// There is a section dedicated to the command table.  This macro adds
+// entries to it.
+
+// This way we avoid having a single file with a giant command table. 
+// Instead the command registration and help can be placed right next to the
+// command function
 
 // __attribute__((used)) prevents compiler from optimizing unreferenced data out
 // __attribute__((unused)) prevents compiler from complaining about unused variables
+
+// Note that command table ends up in data section, in RAM.  This is intentional: so that it can be sorted.
+// (otherwise it could be in constants section, in flash only).
 
 #define COMMAND(func, text) \
 static struct console_cmd cmd_entry_ ## func __attribute__ ((section (".COMMAND_TABLE"))) __attribute__((unused)) __attribute__((used)) = \
@@ -48,6 +56,8 @@ static struct console_cmd cmd_entry_ ## func __attribute__ ((section (".COMMAND_
 // (this submits a task, so prompt is issued on next return to sched)
 void nk_init_cli(void);
 
+extern int nk_cli_tid; // tid for CLI task
+
 extern int facmode; // Set if we are in factory mode
 
 // Re-enable CLI: this will print the prompt and request more input
@@ -56,9 +66,16 @@ void nk_cli_enable();
 // Disable the CLI- it will not request more commands from user
 void nk_cli_disable();
 
-extern int nk_cli_tid;
-
 // Print help for a specific command
 int nk_help(const char *name);
+
+// Complete a command
+
+// list_flag = 0: return next character to complete command, ' ' if is
+// command already complete, -1 if there are none or -2 for multiple matches
+
+// list_flag = 1: show all possible completions
+
+int nk_complete(const char *s, int list_flag);
 
 #endif
